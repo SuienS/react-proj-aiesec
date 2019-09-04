aadc951efdc941cb04b69538d597ff7f0435901b
SuienS <ravidu.rocks@gmail.com>
2019-09-04 20:34:17 +0530

    Updates

---
+++ precache-manifest.8b5eee4b076ff9722a2adde7e22f4f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03997a635f2a5025fd3c4d795ed6e419",
+    "url": "/react-proj-aiesec/index.html"
+  },
+  {
+    "revision": "be71ff26b7b3497bcbeb",
+    "url": "/react-proj-aiesec/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "10c315d7a8322c4528c3",
+    "url": "/react-proj-aiesec/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "be71ff26b7b3497bcbeb",
+    "url": "/react-proj-aiesec/static/js/2.579f7e88.chunk.js"
+  },
+  {
+    "revision": "10c315d7a8322c4528c3",
+    "url": "/react-proj-aiesec/static/js/main.06ea7263.chunk.js"
+  },
+  {
+    "revision": "2fd59a7eea74cf9340e3",
+    "url": "/react-proj-aiesec/static/js/runtime~main.ae958d14.js"
+  }
+]);
