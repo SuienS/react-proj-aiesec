5d3f5914170a4308efd28c5f737224957a67e04c
SuienS <ravidu.rocks@gmail.com>
2019-09-05 10:35:51 +0530

    Correction HTTPS

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
   //PATCH request for editing opportunity data
   editOpportunity(id, title) {
 
-    let reqInfo = 'http://api-staging.aiesec.org/v2/opportunities/';
+    let reqInfo = 'https://api-staging.aiesec.org/v2/opportunities/';
     let accTok = '?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c';
 
     axios.get(reqInfo + id + accTok).then((response) => {
